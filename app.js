4eb55e4a2ad91275bbc2ae9f6ca0fd87cc41ea97
dahaden <dahaden@gmail.com>
2014-03-18 23:20:39 +1300

    pulls from html page

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function(request, response) {
-  // Answer goes here.
-  response.writeHead(200);
-  response.end("Hello World!");
+  fs.readFile('html/losSignup.html', function(err, content) {
+    response.writeHead(200, {
+  'Content-Length': content.length,
+  'Content-Type': 'text/html' });
+    response.write(content);
+
+    response.end();
+  });
 }).listen(8080);
 
 console.log("Listening to port 8080");
