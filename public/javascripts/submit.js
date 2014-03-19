010d713524b17c2c73eecb7f0c3c9f02cf6750f1
dahaden <dahaden@gmail.com>
2014-03-19 22:22:10 +1300

    Just need to by pass cross domain posting...

--- public/javascripts/submit.js
+++ public/javascripts/submit.js
@@ -1,23 +1,30 @@
 var createChar = function() {
     //var data = { name:"Barney", token:"12345" };
-    var name = document.getElementById("input").value;
+    var chname = document.getElementById("input").value;
 
-    if(name == ""){
+    if(chname == ""){
       warning("You give me nothing? I give you nothing!");
       return;
     }
 
-    var token = rand(9);
+    var chtoken = rand(9);
+
+    var toSend = JSON.stringify({ name:chname, token:chtoken });
+
     $.ajax({
     type: 'POST',
     url: 'http://aurora.quantonz.com:9000/adventurers',
     crossDomain: true,
-    data: '{"name":"json", "token":token}',
+    data: toSend,
     dataType: 'json',
     success: function(responseData, textStatus, jqXHR) {
         var value = responseData.someKey;
         $("#submit").toggle(200);
         $("#confirm").toggle(200);
+
+        $("#name").html($("#name").html() + " " + chname);
+        $("#token").html($("#token").html() + " " + chtoken);
+
         if($("#warning").css("display") != "none") {
           $("#warning").toggle(200);
         }
