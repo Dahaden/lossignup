be156e859dd7dd7c039f91cd9c102efd0237d30d
Dahaden <dhad933@aucklanduni.ac.nz>
2014-03-20 22:16:56 +1300

    changed port no

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -15,7 +14,7 @@ var app = express();
 
 
 // all environments
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 9001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
